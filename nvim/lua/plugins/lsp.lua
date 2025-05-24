a0f825b14ba1f6eba67f41a8a1287ebd97bca65e
JesusTM <jesustapiamartin@gmail.com>
2025-05-23 22:48:54 -0400

    feat(lsp): agregar configuración inicial y automatización con Mason
    - Se añade la configuración básica para servidores LSP comunes en LazyVim y se asegura su instalación automática usando mason-lspconfig.

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      tsserver = {},
+      pyright = {},
+      jdtls = {},
+      html = {},
+      cssls = {},
+      clangd = {},
+      dockerls = {},
+      docker_compose_language_service = {},
+      intelephense = {},
+    },
+  },
+}
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "ts_ls",
+      "pyright",
+      "jdtls",
+      "html",
+      "cssls",
+      "clangd",
+      "dockerls",
+      "docker_compose_language_service",
+      "intelephense",
+    },
+  },
+}
